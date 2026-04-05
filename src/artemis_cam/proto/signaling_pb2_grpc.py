8fa98f9a8876a581def35a05057f0b93c4a880ef
wcy <wcy@gmail.com>
2026-04-05 14:18:56 +0000

    fix

--- src/artemis_cam/proto/signaling_pb2_grpc.py
+++ src/artemis_cam/proto/signaling_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from artemis_cam.proto import signaling_pb2 as artemis__cam_dot_proto_dot_signaling__pb2
 
-GRPC_GENERATED_VERSION = '1.80.0'
+GRPC_GENERATED_VERSION = '1.78.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
